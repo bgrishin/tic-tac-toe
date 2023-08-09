6d69eb18647f4860075827b5151b93f22a34c7e0
bgrishin <bogdan.grishin.2007@gmail.com>
2023-08-09 20:01:12 +0200

    pwa fix

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,9 @@
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open('pwa-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                'index.html',
-                'manifest.json'
-            ]);
-        })
-    );
+self.addEventListener('install', function() {
+    console.log('Install!');
 });
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("activate", event => {
+    console.log('Activate!');
+});
+self.addEventListener('fetch', function(event) {
+    console.log('Fetch!', event.request);
 });
